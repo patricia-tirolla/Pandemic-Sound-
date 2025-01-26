efca706eec55cc85fa360db1e8c90cb1ab5b8487
Natito9 <179492180+Natito9@users.noreply.github.com>
2025-01-26 16:36:43 +0100

    added main files jsx and css

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Homepage from './Components/Homepage/Homepage';
+import Nav from './Components/Nav/Nav';
+import Sidebar from './Components/Sidebar/Sidebar';
+import './styles/App.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+        <Nav/>
+        <Sidebar/>
+        <Homepage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Homepage/Homepage.jsx
@@ -0,0 +1,11 @@
+import "./homepage.css"
+
+function Homepage() {
+    return (
+      <div className="homepage">
+        <h1>Good Morning</h1>
+      </div>
+    );
+  }
+  
+  export default Homepage;
