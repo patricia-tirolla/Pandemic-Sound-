b57f7334d613588d186fdbdca0ae1099540d6caf
Natalia Torres <179492180+Natito9@users.noreply.github.com>
2025-01-27 11:54:47 +0100

    Api merged with files (#3)
    * added main files jsx and css
    
    * app style layout and root colors
    
    * main style for sidebar and background
    
    * main nav style
    
    * main homepage style
    
    * back to the past
    
    * merged api and main files

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
-import './App.css';
+import Homepage from './Components/Homepage/Homepage';
+import Nav from './Components/Nav/Nav';
+import Sidebar from './Components/Sidebar/Sidebar';
+import './styles/App.css';
 
-import SpotifyAuth from './Api-auth/ApiAuthComponent';
+
+
+
+
 
 function App() {
-  const clientId = "bab8a1bc1b6348759c3cd4efb8b959e9";
 
   return (
     <div className="App">
+        <Nav/>
+        <Sidebar/>
+        <Homepage/>
       
-      <main>
-        <SpotifyAuth clientId={clientId} />
-      </main>
+
     </div>
+   
   );
 }
 
 export default App;
+
