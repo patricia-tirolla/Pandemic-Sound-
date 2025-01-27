6aa3970db3f4e05ab458e6b38db6eb225315ffe7
ffups <142579057+ffups@users.noreply.github.com>
2025-01-27 14:19:44 +0100

    bugfixes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Homepage from './Components/Homepage/Homepage';
 import Nav from './Components/Nav/Nav';
 import Sidebar from './Components/Sidebar/Sidebar';
 import './styles/App.css';
-
+import React from 'react';
 
 
 
--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,5 +1,5 @@
+import React from "react";
 import "./nav.css";
-
 function Nav() {
     return (
       <div className="Nav">
