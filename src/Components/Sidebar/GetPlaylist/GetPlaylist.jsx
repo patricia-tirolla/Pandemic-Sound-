f46ca612915931076a5d11e05e40b236595c70d1
Natalia Torres <179492180+Natito9@users.noreply.github.com>
2025-01-30 10:59:30 +0100

    Playlist giving props : track URL (#10)
    * added main files jsx and css
    
    * app style layout and root colors
    
    * main style for sidebar and background
    
    * main nav style
    
    * main homepage style
    
    * back to the past
    
    * merged api and main files
    
    * fetched playlist
    
    * saved playlists items in state
    
    * displayed user playlists name and number of songs
    
    * fetchingplaylist working
    
    * hover and active on sidebar playlist container
    
    * router for playlists
    
    * half done wih context tracks trials
    
    * created button to add new playlist if there is no playlists
    
    * merged with songs
    
    * track url to local storage and as props to displayPlaylist

--- src/Components/Sidebar/GetPlaylist/GetPlaylist.jsx
+++ src/Components/Sidebar/GetPlaylist/GetPlaylist.jsx
@@ -1,51 +1,56 @@
 import "./getPlaylist.css";
-
-import React, { useEffect, useState,useContext} from "react";
+import PlaylistDisplay from "../../PlaylistDisplay/PlaylistDisplay";
+import React, { useEffect, useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { ProfileContext } from "../../../contexts";
 
-
 const GetPlaylist = () => {
-
-  const profile = useContext(ProfileContext) 
+  const profile = useContext(ProfileContext);
 
   const [loading, setLoading] = useState(true);
   const [data, setData] = useState([]);
   const navigate = useNavigate();
 
   useEffect(() => {
-     
-        const token = localStorage.getItem("accessToken");
-       
-        const playlistParameters = {
-          method: "GET",
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        };
+    const token = localStorage.getItem("accessToken");
 
-        if (profile !== null) {
-          const playlistAPI = `https://api.spotify.com/v1/users/${profile.id}/playlists`;
-     
-          fetch(playlistAPI, playlistParameters)
-         .then(response => response.json())
-         .then(playlist => setData(playlist.items))
-         .catch(error=> console.log("Error fetching playlists:", error)) 
-        
-        .finally(()=>setLoading(false))
-        }
-   
-    },
-    [profile]);
+    const playlistParameters = {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+
+    if (profile !== null) {
+      const playlistAPI = `https://api.spotify.com/v1/users/${profile.id}/playlists`;
+
+      fetch(playlistAPI, playlistParameters)
+        .then((response) => response.json())
+        .then((playlist) => {
+          console.log(playlist.items);
+          playlist.items.forEach((item) => {
+            const trackUrl = item.tracks.href; 
+            if (trackUrl) {
+              localStorage.setItem(item.id, trackUrl); //passing to localstorage
+            }
+          });
+
+          setData(playlist.items);
+        })
+        .catch((error) => console.log("Error fetching playlists:", error))
+    
+        .finally(() => setLoading(false));
+    }
+  }, [profile]);
+
 
   if (loading) {
     return <h2>Loading...</h2>;
   }
 
   const displayPlaylist = (playlist) => {
-    navigate(`/playlist/${playlist.name}`,{ state: {playlist}});
+    navigate(`/playlist/${playlist.name}`, { state: { playlist } });
   };
-
 
   return (
     <div>
@@ -61,11 +66,15 @@ const GetPlaylist = () => {
               <div className="playlist-info">
                 <h4>{playlist.name}</h4>
                 <p>{playlist.tracks.total} songs</p>
+                <PlaylistDisplay trackUrl={playlist.tracks.href}/>
               </div>
             </div>
           ))
         ) : (
-          <p>No playlists found</p>
+          <div>
+            <p>No playlists found</p>
+            <button>Add new playlist</button>
+          </div>
         )}
       </div>
     </div>
