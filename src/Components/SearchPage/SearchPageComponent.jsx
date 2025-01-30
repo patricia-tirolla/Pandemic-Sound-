1f14939d8b212886dea65590f2f89d172964245d
Patricia Tirolla <patricia.tirolla@hyperisland.se>
2025-01-30 12:40:31 +0100

    Add css to landpage and move css from app to main

--- src/Components/SearchPage/SearchPageComponent.jsx
+++ src/Components/SearchPage/SearchPageComponent.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import SearchBar from "../SearchBar/SearchBarComponent";
+import SearchBar from "./SearchBarComponent";
 import { Link } from "react-router";
 
 const SearchPage = () => {
