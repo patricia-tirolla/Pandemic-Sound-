89c7eef23472fcae28e8b74dbb1048c575ea76c0
Natito9 <179492180+Natito9@users.noreply.github.com>
2025-01-30 10:02:15 +0100

    created button to add new playlist if there is no playlists

--- src/Components/Sidebar/GetPlaylist/GetPlaylist.jsx
+++ src/Components/Sidebar/GetPlaylist/GetPlaylist.jsx
@@ -1,51 +1,45 @@
 import "./getPlaylist.css";
 
-import React, { useEffect, useState,useContext} from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { ProfileContext } from "../../../contexts";
 
-
 const GetPlaylist = () => {
-
-  const profile = useContext(ProfileContext) 
+  const profile = useContext(ProfileContext);
 
   const [loading, setLoading] = useState(true);
   const [data, setData] = useState([]);
   const navigate = useNavigate();
 
   useEffect(() => {
-     
-        const token = localStorage.getItem("accessToken");
-       
-        const playlistParameters = {
-          method: "GET",
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        };
+    const token = localStorage.getItem("accessToken");
 
-        if (profile !== null) {
-          const playlistAPI = `https://api.spotify.com/v1/users/${profile.id}/playlists`;
-     
-          fetch(playlistAPI, playlistParameters)
-         .then(response => response.json())
-         .then(playlist => setData(playlist.items))
-         .catch(error=> console.log("Error fetching playlists:", error)) 
-        
-        .finally(()=>setLoading(false))
-        }
-   
-    },
-    [profile]);
+    const playlistParameters = {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+
+    if (profile !== null) {
+      const playlistAPI = `https://api.spotify.com/v1/users/${profile.id}/playlists`;
+
+      fetch(playlistAPI, playlistParameters)
+        .then((response) => response.json())
+        .then((playlist) => setData(playlist.items))
+        .catch((error) => console.log("Error fetching playlists:", error))
+
+        .finally(() => setLoading(false));
+    }
+  }, [profile]);
 
   if (loading) {
     return <h2>Loading...</h2>;
   }
 
   const displayPlaylist = (playlist) => {
-    navigate(`/playlist/${playlist.name}`,{ state: {playlist}});
+    navigate(`/playlist/${playlist.name}`, { state: { playlist } });
   };
-
 
   return (
     <div>
@@ -65,7 +59,10 @@ const GetPlaylist = () => {
             </div>
           ))
         ) : (
-          <p>No playlists found</p>
+          <div>
+            <p>No playlists found</p>
+            <button>Add new playlist</button>
+          </div>
         )}
       </div>
     </div>
