7e85878532bb59f44b9911fdc736c3d79e2b8f74
swathismenon <swathi.menon@hyperisland.se>
2025-01-31 09:31:52 +0100

    New Release files updated

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,14 +1,28 @@
-import React from "react"
+import React from "react";
 import SearchBar from "../SearchPage/SearchBarComponent";
+import { useProfile } from "../../Hooks/Profile";
+import { isUserAutheticated } from "../AuthCallback/script";
 import "./nav.css";
 
 const Nav = () => {
-    return (
-      <div className="Nav">
-        <h1 className="logo">Pandemic Sound</h1>
-        <SearchBar />
-      </div>
-    );
-  }
-  
-  export default Nav;
+  const profile = useProfile(); 
+
+  return (
+    <div className="Nav">
+      <h1 className="logo">Pandemic Sound</h1>
+      <SearchBar />
+
+      {isUserAutheticated() && profile?.images?.[0]?.url && (
+        <div className="profile-container">
+          <img
+            src={profile.images[0].url}
+            alt="Profile"
+            className="profile-image"
+          />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Nav;
