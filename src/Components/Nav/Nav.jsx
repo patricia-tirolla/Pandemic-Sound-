3dc092f23d35f810d3e66f40ec8f8a85ccba4320
ffups <142579057+ffups@users.noreply.github.com>
2025-01-31 14:52:04 +0100

    added nav home button

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -3,22 +3,26 @@ import SearchBar from "../SearchPage/SearchBarComponent";
 import { useProfile } from "../../Hooks/Profile";
 import { isUserAutheticated } from "../AuthCallback/script";
 import "./nav.css";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
   const profile = useProfile(); 
 
   return (
     <div className="Nav">
-      <h1 className="logo">Pandemic Sound</h1>
-      <SearchBar />
-
+  <Link to="/home" className="logo-link">
+        <h1 className="logo">Pandemic Sound</h1>
+        </Link>
+        <SearchBar />
       {isUserAutheticated() && profile?.images?.[0]?.url && (
         <div className="profile-container">
+      <Link to="/home" >
           <img
             src={profile.images[0].url}
             alt="Profile"
             className="profile-image"
           />
+      </Link>
         </div>
       )}
     </div>
