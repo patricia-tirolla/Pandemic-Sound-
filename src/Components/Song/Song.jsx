b7d2c5711cf1a58e640f26257a6f960b47077094
Patricia Tirolla <179636704+patricia-tirolla@users.noreply.github.com>
2025-01-31 10:16:32 +0100

    Track (#18)
    * Add useFetch hook
    
    * refactor SearchPage with useFetch hook
    
    * Add clickable and preview player to songs

--- src/Components/Song/Song.jsx
+++ src/Components/Song/Song.jsx
@@ -1,17 +1,28 @@
-import React, { useState } from "react";
-import useSpotifyTrackData from "./GetTrackUrl";
+import React, { useState, useEffect } from "react";
+// import useSpotifyTrackData from "./SongHook";
 import "./song.css";
 import { useParams } from "react-router-dom";
+import useFetch from "../../Hooks/useFetch";
 
 const Song = () => {
   const [error] = useState(null);
+  const [embedUrl, setEmbedUrl] = useState();
   const [accessToken] = useState(localStorage.getItem("accessToken"));
-
   const { trackId } = useParams();
-  const { trackData, error: trackError } = useSpotifyTrackData(
-    accessToken,
-    trackId
-  );
+  const { data: trackData, error: trackError } = useFetch(`https://api.spotify.com/v1/tracks/${trackId}`, accessToken);
+
+  useEffect(() => {
+    if (trackData) {
+      console.log(trackData)
+
+      fetch("https://open.spotify.com/oembed?url=" + trackData.external_urls.spotify)
+        .then((resp) => resp.json())
+        .then((json) => setEmbedUrl(json.iframe_url))
+        .catch((err) => console.error(err));
+    }
+  }, [trackData]);
+  console.log(trackData, embedUrl);
+
 
   if (error || trackError) {
     return <div>Error: {error || trackError}</div>;
@@ -25,51 +36,31 @@ const Song = () => {
 
   return (
     <div className="songContainer">
-      {trackData && trackData.tracks && trackData.tracks.map((track) => (
-         <div key={track.id} className="track">
-         {track.album && track.album.images && track.album.images[0] && (
-           <img
-             className="trackImage"
-             src={track.album.images[0].url}
-             alt="Track Art"
-           />
-         )}
-        <h3>{track.name}</h3>
-        <p>Artist: {track.artists.map(artist => artist.name).join(", ")}</p>
-        <p>Album: {track.album.name}</p>
-        <p>Duration: {formatDuration(track.duration_ms)}</p>
-
+      {trackData && (
+        <div className="trackDetails">
+          {/* <img
+            className="trackImage"
+            src={trackData.album.images[0].url}
+            alt="Track Art"
+          /> */}
+          <div className="trackInfo">
+            <p className="trackName">{trackData.name}</p>
+            <p className="trackArtistName">{trackData.artists[0].name}</p>
+            <p className="trackLength">
+              Duration: {formatDuration(trackData.duration_ms)}
+            </p>
             <button className="trackButtonHeart">heart</button>
             <button className="trackButtonAddtoPlaylist">
               add to playlist
             </button>
+            {embedUrl && 
+              <iframe src={embedUrl}></iframe>
+            }
           </div>
-      ))}
-      </div >
+        </div>
+      )}         
+    </div>
   );
 };
 
 export default Song;
-
-// const courses = [
-//   "Full Stack Developement Program",
-//   "Python Automation Testing Program",
-//   "UI/UX Program",
-// ];
-
-// function App(props) {
-
-//   /* Mapping the courses into a new array of JSX nodes as arrayDataItems */
-//   const arrayDataItems = courses.map((course) => <li>{course}</li>);
-
-//   return (
-//     <div className="container">
-//       <div>
-//         <h1>Render List/Array of Items</h1>
-//       </div>
-//       {/* returning arrayDataItems wrapped in <ul> */}
-
-//       <ul>{arrayDataItems}</ul>
-//     </div>
-//   );
-// }
